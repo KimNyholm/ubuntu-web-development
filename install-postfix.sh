9165a6606e8862e3ae4465a98a0a19dd90b98ad8
Kim Nyholm <kim.nyholm.dk@gmail.com>
2015-09-16 23:16:44 +0200

    Comment fixed

--- install-postfix.sh
+++ install-postfix.sh
@@ -5,7 +5,7 @@ sudo debconf-set-selections <<< 'postfix postfix/main_mailer_type select Interne
 sudo debconf-set-selections <<< 'postfix postfix/mailname string ubuntu.dev'
 sudo apt-get --yes install postfix
 
-$install mail stack delivery
+#install mail stack delivery
 sudo debconf-set-selections <<< 'dovecot-core dovecot-core/ssl-cert-name string ubuntu.dev'
 sudo debconf-set-selections <<< 'dovecot-core dovecot-core/create-ssl-cert boolean false'
 sudo apt-get --yes install mail-stack-delivery
