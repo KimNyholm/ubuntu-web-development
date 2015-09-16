23d937c13199692ad8b79bddc4522e745903daf1
Kim Nyholm <mail@kimnyholm.com>
2015-09-16 23:06:48 +0200

    postfix configuration parameters updated

--- install-postfix.sh
+++ install-postfix.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #Install postfix
-sudo debconf-set-selections <<< 'postfix  postfix/main_mailer_type  select  Internet Site'
-sudo debconf-set-selections <<< 'postfix  postfix/destinations  string  ubuntu.dev, localhost.dev, , localhost'
+sudo debconf-set-selections <<< 'postfix postfix/main_mailer_type select Internet Site'
+sudo debconf-set-selections <<< 'postfix postfix/mailname string ubuntu.dev'
 sudo apt-get --yes install postfix
 
 $install mail stack delivery
+sudo debconf-set-selections <<< 'dovecot-core dovecot-core/ssl-cert-name string ubuntu.dev'
+sudo debconf-set-selections <<< 'dovecot-core dovecot-core/create-ssl-cert boolean false'
 sudo apt-get --yes install mail-stack-delivery
