2d9facd14ff09bf9baf555024af3fa4fab639f5e
Kim Nyholm <mail@kimnyholm.com>
2016-04-09 11:09:48 +0000

    Drupal directory name follows sitename.

--- sh/drupalgetversion.sh
+++ sh/drupalgetversion.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 if [ "'$3'" == "''" ] ; then
-  echo "To call '$0 dev|release version database'"
+  echo "To call '$0 dev|release version sitename'"
   exit 1
 fi
 
 dv=$2
-echo "Installing drupal version $dv ($1)"
+sitename=$3
+echo "Installing drupal version $dv ($1) on site $sitename"
 
 pushd /var/www/html/
 if [ "'$1'" == "'dev'" ] ; then
@@ -13,11 +14,11 @@ if [ "'$1'" == "'dev'" ] ; then
   git clone --branch $dv http://git.drupal.org/project/drupal.git
   mv drupal drupal-$dv
 else
-  drush dl drupal-$dv --drupal-project-rename=drupal-$dv --yes
+  drush dl drupal-$dv --drupal-project-rename=$sitename --yes
 fi
 
-cd drupal-$dv
-drush site-install standard --db-url="mysql://ada:Lovelace@localhost/$3" --site-name="Drupal $dv on Trusty" --account-name=ada --account-pass=Lovelace --yes
+cd $sitename
+drush site-install standard --db-url="mysql://ada:Lovelace@localhost/$sitename" --site-name="Drupal $dv site $sitename on Trusty" --account-name=ada --account-pass=Lovelace --yes
 sudo chmod -R 777 sites/default/files
 sudo chown -R :www-data *
 popd
