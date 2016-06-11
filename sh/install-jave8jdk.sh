9b30017b82fcbb1b63ebd71df2b7f0a89305e3e7
Kim Nyholm <mail@kimnyholm.com>
2016-06-11 21:03:16 +0200

    JAVA_HOME added to environment

--- sh/install-jave8jdk.sh
+++ sh/install-jave8jdk.sh
@@ -6,3 +6,6 @@ sudo apt-get update
 
 #install java 8 jdk
 sudo apt-get install oracle-java8-installer
+
+#add JAVA_HOME to environment
+echo JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre/bin/java | sudo tee -a /etc/environment
