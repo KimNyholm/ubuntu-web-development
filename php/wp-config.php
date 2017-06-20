de12d97cce18b77cb6bd37487391851a7025a217
Kim Nyholm <mail@kimnyholm.com>
2017-06-20 13:04:57 +0200

    Making username and password configureable

--- php/wp-config.php
+++ php/wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'ada');
+define('DB_USER', 'USERID');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Lovelace');
+define('DB_PASSWORD', 'USERPW');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
