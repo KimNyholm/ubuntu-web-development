91ffef04ef763aebf8c95eb3689b0777981c7cfb
Kim Nyholm <mail@kimnyholm.com>
2017-06-21 12:40:51 +0200

    vsftpd prepared for Amazon EC2

--- sh/install-vsftpd.sh
+++ sh/install-vsftpd.sh
@@ -4,11 +4,29 @@
 sudo apt-get --yes install vsftpd
 
 #Configure ftp server
-grep -v -i "local_umask=077" /etc/vsftpd.conf >/tmp/vsftpd.tmpa
-grep -v -w -i "write_enable=" /tmp/vsftpd.tmpa >/tmp/vsftpd.tmp
-echo 'local_umask=022' >> /tmp/vsftpd.tmp
-echo 'write_enable=yes' >> /tmp/vsftpd.tmp
+sed '/local_umask=/d' /etc/vsftpd.conf | sed '/write_enable=/d' >/tmp/vsftpdA.tmp
+sed '/listen=/d' /tmp/vsftpdA.tmp | sed '/listen_ipv6=/d' >/tmp/vsftpdB.tmp
+
+# setup for vagrant box
+cat <<OPTIONS >>/tmp/vsftpdB.tmp
+listen=YES
+local_umask=022
+write_enable=YES
+OPTIONS
+
+if [ ! -z $1 ]
+  then
+# additional setup for Amazon EC2
+cat <<OPTIONS >>/tmp/vsftpdB.tmp
+pasv_enable=YES
+pasv_min_port=1024
+pasv_max_port=1048
+pasv_addr_resolve=$1
+port_enable=YES
+OPTIONS
+  fi
+
 #Do not attempt to do an mv instead of copy.
 #vsftpd will fail. File is propably locked due to sharing with host.
-sudo cp /tmp/vsftpd.tmp /etc/vsftpd.conf
+sudo cp /tmp/vsftpdB.tmp /etc/vsftpd.conf
 sudo service vsftpd reload
