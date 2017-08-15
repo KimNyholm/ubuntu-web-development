ae622dd4a7b56d1da32f92f264960b7d2f8bf493
Kim Nyholm <mail@kimnyholm.com>
2017-08-15 13:56:55 +0200

    Node installer updated

--- sh/install-nodejs.sh
+++ sh/install-nodejs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get --yes install nodejs npm
-sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-get --yes install nodejs
