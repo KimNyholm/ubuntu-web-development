8c0335f5b541a211ae376604cf800adadd0a3d54
Kim Nyholm <mail@kimnyholm.com>
2017-10-09 15:05:08 +0200

    Fix home directory

--- sh/install-drush.sh
+++ sh/install-drush.sh
@@ -7,7 +7,7 @@ sudo mv composer.phar /usr/local/bin/composer
 #install drush
 composer global require drush/drush
 sudo ln -s ~/.config/composer/vendor/drush/drush/drush /usr/bin/drush
-sudo chmod -R 777 /home/vagrant/.drush/cache
+sudo chmod -R 777 ~/.drush/cache
 
 #check drush version, will give warning first time
 #	chmod: cannot access '/home/vagrant/.drush/cache': No such file or directory
