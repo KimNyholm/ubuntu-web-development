ec10e113eed91c9a42626fd3cd307a5abdd380e8
Kim Nyholm <mail@kimnyholm.com>
2017-10-09 12:50:14 +0000

    Enable drush install on AWS

--- sh/install-drush.sh
+++ sh/install-drush.sh
@@ -6,8 +6,8 @@ sudo mv composer.phar /usr/local/bin/composer
 
 #install drush
 composer global require drush/drush
-sudo ln -s /home/vagrant/.config/composer/vendor/drush/drush/drush /usr/bin/drush
-sudo chmod -R 777 /home/vagrant/.drush/cache
+sudo ln -s ~/.config/composer/vendor/drush/drush/drush /usr/bin/drush
+sudo chmod -R 777 ~/.drush/cache
 
 #check drush version, will give warning first time
 #	chmod: cannot access '/home/vagrant/.drush/cache': No such file or directory
