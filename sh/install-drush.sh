0e6e6b48e5ac646697d6269c888e8b82a04a71ce
Kim Nyholm <mail@kimnyholm.com>
2018-03-17 11:55:36 +0000

    Drush ensure specific version

--- sh/install-drush.sh
+++ sh/install-drush.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-#install composer
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-
-#install drush
-composer global require drush/drush
-sudo ln -s ~/.config/composer/vendor/drush/drush/drush /usr/bin/drush
-sudo chmod -R 777 ~/.drush/cache
-
-#check drush version, will give warning first time
-#	chmod: cannot access '/home/vagrant/.drush/cache': No such file or directory
+curl https://github.com/drush-ops/drush/releases/download/8.1.15/drush.phar -L -o /tmp/drush.phar
+sudo chmod 777 /tmp/drush.phar
+sudo mv /tmp/drush.phar /usr/local/bin/drush
 drush --version
