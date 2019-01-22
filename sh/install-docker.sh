c7e77e39f26f6e2182594594572eaff3c843e953
Kim Nyholm <mail@kimnyholm.com>
2019-01-22 15:03:03 +0000

    Docker no confirm

--- sh/install-docker.sh
+++ sh/install-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get --yes install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get --yes install docker-ce
 sudo usermod -aG docker ${USER}
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
